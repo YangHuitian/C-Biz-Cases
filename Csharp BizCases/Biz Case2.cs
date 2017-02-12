99975b9f09e0210b2b104f3a9b56e7b1362cf948
Yang <Yang@YANG-N552VW>
2017-02-12 14:57:37 +0800

    finish Biz Cases 123

---
+++ Csharp BizCases/Biz Case2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_BizCases
+{
+    class Biz_Case2
+    {
+        //Biz Cases 2: Data migration: Postal Code
+        //assunming that the old addresss is "Tower 52 Blk 729 West Coast, Singapore 1254"
+
+        static string d3ToNumber(string s)
+        {
+            switch (s)
+            {
+                case "A":
+                    return "1";
+                case "B":
+                    return "2";
+                case "C":
+                    return "3";
+                case "D":
+                    return "4";
+                case "E":
+                    return "5";
+                case "F":
+                    return "6";
+                case "G":
+                    return "7";
+                case "H":
+                    return "8";
+            }
+            return "";
+        }   
+
+        public static void Main()
+        {
+            Console.Write("Your address is: ");
+            string w = Console.ReadLine().ToUpper();
+            string[] s = w.Split(' ');
+
+            string bln = "";
+            string enn = "";
+            for (int i = 0; i <= s.Length -1; i++)
+            {
+                if (s[i] == "BLK" || s[i] == "BlOCK")
+                {
+                    bln = s[i + 1];//"729B"
+                }
+                else if (s[i] == "SINGAPORE")
+                {
+                    enn = s[i + 1];//1254
+                }
+            }
+
+            string d3 = "";
+            if (bln.Length == 4)//exist B
+            {
+                d3 = d3ToNumber(bln.Substring(3, 1));
+            }
+            else//(bln.Length == 3)//not exist B
+            {
+                d3 = "0";
+            }
+
+            string new12 = enn.Substring(2, 2);
+            string new456 = bln.Substring(0, 3);
+            string newcode = new12 + d3 + new456;
+            Console.WriteLine("Your New Postal Code is: {0}", newcode);
+        }
+    }
+}
---
+++ Csharp BizCases/Biz Case3.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_BizCases
+{
+    class Biz_Case3
+    {
+        //Biz Cases3 : Encryption
+
+        static void Encrypt(string s)
+        {
+            string[] fir = new string[s.Length];//create a new[] for change into next alphabet series
+            string ALPH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string alph = "abcdefghijklmnopqrstuvwxyz";
+            string num = "0123456789";
+
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (ALPH.Contains(s[i]))
+                {
+                    for (int j = 0; j <= ALPH.Length - 1; j++)//through out ALPH
+                    {
+                        if (s[i] == ALPH[j] && ALPH[j] != 'Z')
+                        {
+                            fir[i] = ALPH.Substring(j + 1, 1);
+                        }
+                        else if (s[i] == ALPH[j] && ALPH[j] == 'Z')
+                        {
+                            fir[i] = "A";
+                        }
+                    }
+                }
+                else if (alph.Contains(s[i]))
+                {
+                    for (int p = 0; p <= alph.Length - 1; p++)//through out alph
+                    {
+                        if (s[i] == alph[p] && alph[p] != 'z')
+                        {
+                            fir[i] = alph.Substring(p + 1, 1);
+                        }
+                        else if (s[i] == alph[p] && alph[p] == 'z')
+                        {
+                            fir[i] = "a";
+                        }
+                    }
+                }
+                else//num Contains s[i]
+                {
+                    for (int n = 0; n <= num.Length - 1; n++)//through out num
+                    {
+                        if (s[i] == num[n] && n != 9)
+                        {
+                            fir[i] = num.Substring(n + 1, 1);
+                        }
+                        else if (s[i] == num[n] && n == 9)
+                        {
+                            fir[i] = "0";
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(String.Join("", fir));
+
+            string[] sec = new string[fir.Length];//create a new[] for Mirror Image Change
+            for (int u = 0; u <= fir.Length - 1; u++)//123opQ
+            {
+                sec[u] = fir[5-u];
+            }
+
+            Console.WriteLine(String.Join("", sec));
+        }   
+
+
+        public static void Main()
+        {
+            Console.Write("Pls enter your password: ");
+            string password = Console.ReadLine();
+            Encrypt(password);
+        }
+    }
+}
